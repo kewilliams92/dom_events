f34b00c7409198d2be761503d9a581769920e371
sinroth <kewilliams2017@hotmail.com>
2022-10-12 17:39:17 -0400

    finished cookie clicker

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+let x = 0
+document.querySelector("#counter").innerText = x
+document.querySelector(".cookie").addEventListener("click", iterate)
 
+function iterate(){
+    x += 1;
+    console.log(x);
+    document.querySelector("#counter").innerText = x;
+}
+
+document.querySelector(".text").addEventListener("click", largerText())
+document.querySelector(".text").addEventListener("click", smallerText())
+
+
+//the text should get larger upon user clicking.  the text should not get larger then 100px.  we will use the value of 16px to increment size.
