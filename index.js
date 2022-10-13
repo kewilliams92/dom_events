e708104a239e245aefc36880b888e864227da1d7
sinroth <kewilliams2017@hotmail.com>
2022-10-12 21:29:14 -0400

    finished magnifying glass

--- index.js
+++ index.js
@@ -8,8 +8,32 @@ function iterate(){
     document.querySelector("#counter").innerText = x;
 }
 
-document.querySelector(".text").addEventListener("click", largerText())
-document.querySelector(".text").addEventListener("click", smallerText())
 
+// // document.querySelector(".text").addEventListener("click", smallerText())
 
 //the text should get larger upon user clicking.  the text should not get larger then 100px.  we will use the value of 16px to increment size.
+document.querySelector(".button1").addEventListener("click", largerText)
+
+document.querySelector(".button2").addEventListener("click", smallerText)
+
+function largerText(){
+    txt = document.querySelector("#random1");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size')
+    currentSize = parseFloat(style);
+    if ((currentSize + 8) > 100){
+        txt.style.fontSize = "100px"
+    } else {
+        txt.style.fontSize = (currentSize + 8) + "px";
+    }
+}
+
+function smallerText(){
+    txt = document.querySelector("#random1");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size')
+    currentSize = parseFloat(style);
+    if ((currentSize - 1) < 1){
+        txt.style.fontSize = "1px"
+    } else {
+        txt.style.fontSize = (currentSize - 1) + "px";
+    }
+}
